9bf36e2fa1b03f7a3345fc3c7583334d641d72de
ReasonableFailure <dinhfaye@protonmail.com>
2025-03-03 11:55:07 +0100

    aaaaaaaaaaaaaa

--- Exercise2/NNSearch_template.py
+++ Exercise2/NNSearch_template.py
@@ -1,7 +1,7 @@
 from heapq import *
 import numpy as np
+from matplotlib import pyplot as plt
 from Exercise_1 import exercise_1 as ex
-
 # Priority queue
 class prioq:
     def __init__(self, k):
@@ -27,9 +27,6 @@ def celldist2(self, r):
     d1 = np.maximum(d1, d2)
     d1 = np.maximum(d1, np.zeros_like(d1))
     return d1.dot(d1)
-    
-k = 32
-pq = prioq(k)
 
 def isLeaf(cell:ex.Cell):
     return (cell.leftChild==None) and (cell.rightChild==None)
@@ -49,11 +46,35 @@ def neighbour_search(pq:prioq, root:ex.Cell, particles:ex.Particle, r, rOffset):
         if -celldist2(root.leftChild,r)>pq.key():
             neighbour_search(pq=pq,root=root.leftChild,particles=particles,r=r,rOffset=rOffset)
 
-
-
 def neighbour_search_periodic(pq, root, particles, r, period):
     # walk the closest image first (at offset=[0, 0])
     for y in [0.0, -period[1], period[1]]:
         for x in [0.0, -period[0], period[0]]:
             rOffset = np.array([x, y])
             neighbour_search(pq, root, particles, r, rOffset)
+
+def queue_plotter(pq:prioq, r, period, axis, color = 'red'):
+    allneighbours = np.array([p[1].r for p in pq.heap])
+    print(allneighbours)
+
+if __name__ == "__main__":
+    A: np.ndarray = np.array([])
+    for _ in range(1000):
+        p = ex.Particle(np.random.rand(2))
+        A = np.append(A, np.array(p))
+    root = ex.Cell(rLow=np.array([0.0, 0.0]),rHigh=np.array([1.0, 1.0]),lo=0,hi=len(A) - 1)
+    ex.plot_particles(A=A)
+    ex.tree_builder(root,A,0)
+    pq = prioq(32)
+    middle_point = ex.Particle(np.array([0.5,0.5]))
+    neighbour_search_periodic(pq=pq,root=root,particles=A,r=middle_point,period=np.array([1.0,1.0]))
+    pq2 = prioq(32)
+    far_point = ex.Particle(np.array([0.8,0.9]))
+    neighbour_search_periodic(pq2,root,A,far_point,np.array([1.0,1.0]))
+    fig, ax = plt.subplots()
+    queue_plotter(pq, middle_point, np.array([1.0,1.0]), ax)
+    queue_plotter(pq2, far_point, np.array([1.0,1.0]), ax, color='green')
+    ax.set_aspect('equal', 'box')
+    ax.set_ylim(0,1)
+    ax.set_xlim(0,1)
+    plt.show()
--- Exercise_1/exercise_1.py
+++ Exercise_1/exercise_1.py
@@ -41,41 +41,76 @@ def partition(A: np.array,i: int, j:int, v:float,d:bool) -> int :
     
 def tree_builder(root: Cell, A: np.array, dim: int):
     v = 0.5 * (root.lowerBound[dim] + root.upperBound[dim])
-    s = partition(A, root.index_low, root.index_high, v, dim)
-
-    # New cell bounds are set depending on the dimension.
+    pivot_index = partition(A, root.index_low, root.index_high, v, dim)
+    print(f"pivot_index = {pivot_index}")
+    #initialise left child cell
+    #split in x direction
     if dim == 0:
-        rLow_Left = root.lowerBound
-        rHigh_Left = np.array([v, root.upperBound[1]])
-        rLow_Right = np.array([v, root.lowerBound[1]])
-        rHigh_Right = root.upperBound
+        ll_l = root.lowerBound
+        ur_l = np.array([v,root.upperBound[1]])
+    #split in y direction
     else:
-        rLow_Left = root.lowerBound
-        rHigh_Left = np.array([root.upperBound[0], v])
-        rLow_Right = np.array([root.lowerBound[0], v])
-        rHigh_Right = root.upperBound
+        ll_l = root.lowerBound
+        ur_l = np.array([root.upperBound[0],v])
 
-    # The left cell is generated if a left partition exists and the branching continued.
-    if s > root.index_low:
-        left_cell = Cell(rLow_Left, rHigh_Left, root.index_low, s - 1)
-        root.leftChild = left_cell
-        if len(A[root.index_low:s]) > 8:
-            tree_builder(root=left_cell,A=A,dim=(1-dim)) # alternate splitting dimensions
+    
+    #initialise right child
+    #split along x axis
+    if dim == 0:
+        ll_r = np.array([v,root.lowerBound[1]])
+        ur_r = root.upperBound
+    else:
+        ll_r = np.array([root.lowerBound[0],v])
+        ur_r = root.upperBound
+    
+    leftChild = Cell(ur_l,ll_l,root.index_low,pivot_index)
+    worth_it_left = pivot_index - root.index_low
+    print(f"worth_it_left = {worth_it_left}")
+    if worth_it_left > 8:
+        root.leftChild = leftChild
+        tree_builder(leftChild,A,(1-dim))
+    
+    rightChild = Cell(ur_r,ll_r,pivot_index+1,root.index_high)
+    worth_it_right = root.index_high - pivot_index -1
+    print(f"worth_it_right = {worth_it_right}")
+    if worth_it_right > 6:
+        root.rightChild = rightChild
+        tree_builder(rightChild,A,(1-dim))
 
-    # The right cell is generated if a right partition exists and then recursed into.
-    if s <= root.index_high:
-        right_cell = Cell(rLow_Right, rHigh_Right, s, root.index_low)
-        root.upperCell = right_cell
-        if len(A[s:root.index_high + 1]) > 8:
-            tree_builder(root=right_cell,A=A,dim=(1-dim)) # alternate splitting dimensions
+    # # New cell bounds are set depending on the dimension.
+    # if dim == 0:
+    #     rLow_Left = root.lowerBound
+    #     rHigh_Left = np.array([v, root.upperBound[1]])
+    #     rLow_Right = np.array([v, root.lowerBound[1]])
+    #     rHigh_Right = root.upperBound
+    # else:
+    #     rLow_Left = root.lowerBound
+    #     rHigh_Left = np.array([root.upperBound[0], v])
+    #     rLow_Right = np.array([root.lowerBound[0], v])
+    #     rHigh_Right = root.upperBound
+
+    # # print(f"rLowLeft = {rLow_Left}, rHighLeft = {rHigh_Left}, rLowRight = {rLow_Right}, rHighRight = {rHigh_Right}")
+    # # The left cell is generated if a left partition exists and the branching continued.
+    # if s > root.index_low:
+    #     left_cell = Cell(rLow_Left, rHigh_Left, root.index_low, s - 1)
+    #     root.leftChild = left_cell
+    #     if len(A[root.index_low:s]) > 8:
+    #         tree_builder(root=left_cell,A=A,dim=(1-dim)) # alternate splitting dimensions
+
+    # # The right cell is generated if a right partition exists and then recursed into.
+    # if s <= root.index_high:
+    #     right_cell = Cell(rLow_Right, rHigh_Right, s, root.index_high)
+    #     root.rightChild = right_cell
+    #     if len(A[s:root.index_high + 1]) > 8:
+    #         # print("here")
+    #         tree_builder(root=right_cell,A=A,dim=(1-dim)) # alternate splitting dimensions
 
 
 def plot_particles(A:np.ndarray[Particle]):
-    plt.scatter([p.r[0] for p in A], [p.r[1] for p in A], color="black")
+    plt.scatter([p.r[0] for p in A], [p.r[1] for p in A], color="black", s=2)
 
 def recursive_tree_plotter(root: Cell):
-    if(root.leftChild):
-        recursive_tree_plotter(root=root.leftChild)
+    
     if(root.rightChild):
         recursive_tree_plotter(root=root.rightChild)
     xl = root.lowerBound[0]
@@ -86,13 +121,17 @@ def recursive_tree_plotter(root: Cell):
     plt.plot([xl, xh], [yh, yh], color="red")
     plt.plot([xl, xl], [yl, yh], color="red")
     plt.plot([xh, xh], [yl, yh], color="red")
+    if(root.leftChild):
+        recursive_tree_plotter(root=root.leftChild)
 
 if __name__ == "__main__":
     A: np.ndarray = np.array([])
     for _ in range(1000):
-        A = np.append(A, np.array(Particle(np.random.rand(2))))
+        p =Particle(np.random.rand(2))
+        A = np.append(A, np.array(p))
     
     root = Cell(rLow=np.array([0.0, 0.0]),rHigh=np.array([1.0, 1.0]),lo=0,hi=len(A) - 1)
     plot_particles(A=A)
     tree_builder(root,A,0)
     recursive_tree_plotter(root=root)
+    plt.show()
