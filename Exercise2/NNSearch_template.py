3990bb34e03255fed7adb8577a4159109b37d2b8
ReasonableFailure <dinhfaye@protonmail.com>
2025-03-03 09:12:02 +0100

    fill out template

--- Exercise2/NNSearch_template.py
+++ Exercise2/NNSearch_template.py
@@ -1,5 +1,6 @@
 from heapq import *
 import numpy as np
+from Exercise_1 import exercise_1 as ex
 
 # Priority queue
 class prioq:
@@ -9,29 +10,48 @@ class prioq:
         for i in range(k):
             heappush(self.heap, sentinel)
 
-    def replace(self, dist2, particle, dr):
-        
-        # .... use heapreplace here
-        pass
+    def replace(self, dist2:float, particle:ex.Particle, dr):
+        insert_node = (dist2,particle,dr)
+        former_closest = heapreplace(self.heap,insert_node)
+        return former_closest
 
     def key(self):
         # .... define key here
-        pass
-
+        minimum_node = self.heap[0]
+        return minimum_node[0] #Distance in the tuple.
 def celldist2(self, r):
     """Calculates the squared minimum distance between a particle
     position and this node."""
-    d1 = r - self.rHigh
-    d2 = self.rLow - r
+    d1 = r - self.upperBound
+    d2 = self.lowerBound - r
     d1 = np.maximum(d1, d2)
     d1 = np.maximum(d1, np.zeros_like(d1))
     return d1.dot(d1)
-def neighbour_search(pq:prioq, root, particles, r, rOffset):
-    pass
-
+    
+k = 32
 pq = prioq(k)
 
-def neighbor_search_periodic(pq, root, particles, r, period):
+def isLeaf(cell:ex.Cell):
+    return (cell.leftChild==None) and (cell.rightChild==None)
+        
+
+def neighbour_search(pq:prioq, root:ex.Cell, particles:ex.Particle, r, rOffset): #this is ball_walk
+    cnt = 0
+    if isLeaf(root):
+        for part in particles[root.index_low:root.index_high+1]:
+            delta = part.r + rOffset - r
+            distance = - delta.dot(delta)
+            if distance > pq.key():
+                formerly = pq.replace(distance,part,delta)
+    else:
+        if -celldist2(root.rightChild,r-rOffset)>pq.key():
+            neighbour_search(pq=pq,root=root.rightChild,particles=particles,r=r,rOffset=rOffset)
+        if -celldist2(root.leftChild,r)>pq.key():
+            neighbour_search(pq=pq,root=root.leftChild,particles=particles,r=r,rOffset=rOffset)
+
+
+
+def neighbour_search_periodic(pq, root, particles, r, period):
     # walk the closest image first (at offset=[0, 0])
     for y in [0.0, -period[1], period[1]]:
         for x in [0.0, -period[0], period[0]]:
